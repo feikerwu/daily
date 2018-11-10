4b09a2d4cb7ac6e8b1008e45783834faf7ca854b
minfield <reFugd?3e@gmail.com>
2018-11-10 15:25:47 +0800

    bugfix

---
+++ day7_leetcode_102.js
@@ -0,0 +1,11 @@
+const levelOrder = function(root) {
+		let res = new Array()
+		let curLevel = [root]
+		let nextLevel = new Array()
+		let nextLevelVals = new Array()
+		res.push([root.val])
+		while (curLevel.length) {
+				
+		}
+}
+
