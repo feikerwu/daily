23b429dc3eacc6243b34d5b458b6bc50c57e8f8f
minfield <feikerwu@gmail.com>
2018-11-15 14:55:40 +0800

    2018.11.15

--- leetcode/19.remove-nth-node-from-end-of-list.js
+++ leetcode/19.remove-nth-node-from-end-of-list.js
@@ -11,24 +11,24 @@
  *
  * Given a linked list, remove the n-th node from the end of list and return
  * its head.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given linked list: 1->2->3->4->5, and n = 2.
- * 
+ *
  * After removing the second node from the end, the linked list becomes
  * 1->2->3->5.
- * 
- * 
+ *
+ *
  * Note:
- * 
+ *
  * Given n will always be valid.
- * 
+ *
  * Follow up:
- * 
+ *
  * Could you do this in one pass?
- * 
+ *
  */
 /**
  * Definition for singly-linked list.
@@ -38,10 +38,26 @@
  * }
  */
 /**
+ * @algorithm: 用两个指针同时向前，两个指针之前的gap为n，先走的指针到达尾部，而后走的指针刚好到n的位置
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    
+    let sentryEle = new ListNode(0)
+    sentryEle.next = head
+    let firstPos = sentryEle,
+        secondPos = sentryEle
+
+    let step = 0
+    while (firstPos.next) {
+        firstPos = firstPos.next
+        step += 1
+        if (step > n) {
+            secondPos = secondPos.next
+        }
+    }
+    secondPos.next = secondPos.next.next
+
+    return sentryEle.next
 };
