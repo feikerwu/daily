a66ebb28ad5c9522af8a804874cc305ecb4e1563
wufei <wufei2@bigo.sg>
2019-07-09 12:25:37 +0800

    add 1234-to-4321-with-recursive.js

---
+++ interview/1234-to-4321-with-recursive.js
@@ -0,0 +1,31 @@
+/**
+ * 输入整型 1234，返回字符串“4321”。
+ * 要求必须使用递归函数调用，不能用全局变量，输入函数必须只有一个参数传入，必须返回字符串。
+ */
+
+/**
+ * @case 1234 => '4321'
+ * @case 1000 => '1'
+ * @case -1000 => '-1'
+ * @case -1234 => '-4321'
+ *
+ * @param {number} number 传入的数值
+ */
+function solution(number) {
+  if (number < 0) {
+    return `-${solution(Math.abs(number))}`;
+  }
+
+  if (number < 10) {
+    return `${number}`;
+  }
+
+  return `${number % 10 || ''}${solution(~~(number / 10))}`;
+}
+
+const assert = require('assert').strict;
+
+assert.strictEqual(solution(1234), '4321');
+assert.strictEqual(solution(1000), '1');
+assert.strictEqual(solution(-1234), '-4321');
+assert.strictEqual(solution(-1000), '-1');
