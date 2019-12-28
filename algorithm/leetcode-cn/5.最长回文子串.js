d8d981be2fa98f8170c4367423a88f777cec8df3
feikerwu <feikerwu@gmail.com>
2019-12-28 23:34:26 +0800

    feat:  5.最长回文子串.js

--- algorithm/leetcode-cn/5.最长回文子串.js
+++ algorithm/leetcode-cn/5.最长回文子串.js
@@ -10,20 +10,34 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  let res = '';
-  for (let i = 0; i < s.length; i++) {
-    for (let j = 0; j <= Math.min(i, s.length - i - 1); j++) {}
+  if (s.length <= 0) {
+    return s;
   }
+
+  let res = s.substring(0, 1);
+
+  for (let i = 0; i < s.length; i++) {
+    let subStr1 = getPalindrome(s, i, i + 1);
+    let subStr2 = getPalindrome(s, i - 1, i + 1);
+    res = res.length < subStr1.length ? subStr1 : res;
+    res = res.length < subStr2.length ? subStr2 : res;
+  }
+
   return res;
 };
 
-function isPalindrome(s) {
-  return (
-    s ===
-    s
-      .split('')
-      .reverse()
-      .join('')
-  );
+function getPalindrome(s, start, end) {
+  if (s[start] !== s[end]) {
+    return '';
+  }
+
+  while (start >= 0 && end < s.length && s[start] === s[end]) {
+    start = start - 1;
+    end = end + 1;
+  }
+
+  return s.substring(start + 1, end);
 }
+
 // @lc code=end
+let res = longestPalindrome('babad');
