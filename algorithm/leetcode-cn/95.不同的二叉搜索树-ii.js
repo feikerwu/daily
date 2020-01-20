b41449f187e620f28c989a9fbcbea24afb148df0
feikerwu <feikerwu@gmail.com>
2020-01-20 11:15:13 +0800

    feat: 98.验证二叉搜索树

--- algorithm/leetcode-cn/95.不同的二叉搜索树-ii.js
+++ algorithm/leetcode-cn/95.不同的二叉搜索树-ii.js
@@ -17,30 +17,13 @@
  * @return {TreeNode[]}
  */
 var generateTrees = function(n) {
-  let dp = Array.from({length: n + 1}).map(item => {
-    return []
-  })
-  dp[0].push([null])
-  dp[1].push([1])
+  let dp = new Array(n+1).fill(null)
+  dp[1] = new TreeNode(1)
   for(let i = 2; i <= n; i++) {
-    for (let j = 0; j < i; j++) {
-      let l = dp[j]
-      let r = dp[i - j - 1]
-      let term = []
-      for (let k = 0; k < l.length; k++) {
-        for (let m = 0; m < r.length; m++) {
-          let l_c = l[k]
-          let r_c = r[m].map(item => item ? item - i : item)
-          term.push([j+1, ...l_c, ...r_c])
-        }
-      }
-      console.log({term})
-      dp[i].push(term)
+    for (let j = 1; j <= i; j++) {
     }
   }
-  // console.log(dp[n])
-  return dp[n]
 };
 // @lc code=end
-console.log(generateTrees(2))
+console.log(generateTrees(3))
 
