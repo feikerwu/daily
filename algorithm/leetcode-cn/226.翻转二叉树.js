0f11aaaa3022528a9d0d754b084d28e09d53d4c2
feikerwu <feikerwu@gmail.com>
2020-01-29 21:36:54 +0800

    feat: 226

---
+++ algorithm/leetcode-cn/226.翻转二叉树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root) {
+    return root
+  }
+
+  let left =  invertTree(root.left)
+  let right = invertTree(root.right)
+
+  root.left = right
+  root.right = left
+
+  return root
+};
+// @lc code=end
+
