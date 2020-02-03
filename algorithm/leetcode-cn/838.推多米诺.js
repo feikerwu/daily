55a7def2e97f7dd302fc34bd1993ba94e7f6b72d
feikerwu <feikerwu@gmail.com>
2020-02-03 23:53:10 +0800

    feat: 455

---
+++ algorithm/leetcode-cn/838.推多米诺.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=838 lang=javascript
+ *
+ * [838] 推多米诺
+ */
+
+// @lc code=start
+/**
+ * @param {string} dominoes
+ * @return {string}
+ */
+var pushDominoes = function(dominoes) {
+    
+};
+// @lc code=end
+
